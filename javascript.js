239b396df0abf9712d397146f94bfa99aba50bf9
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-16 17:00:13 +0000

    Removes unnecessary functions, adds player buttons

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
+const rockButton = document.getElementById("rockBtn");
+const paperButton = document.getElementById("paperBtn");
+const scissorsButton = document.getElementById("scissorsBtn");
+const resultSection = document.getElementById("container");
+
 let humanScore = 0;
 let pooPooCaCaScore = 0;
 
-function getHumanChoice() {
-  let hoomanChoice = prompt(
-    "Type 'Rock', 'Paper' or 'Scissors'",
-    `${getComputerChoice()}`
-  );
-  switch (hoomanChoice.toLowerCase().trim()) {
-    case "rock":
-      return "Rock";
-    case "paper":
-      return "Paper";
-    case "scissors":
-      return "Scissors";
-    default:
-      alert("You didn't pick a valid option");
-  }
-} // Hooman choice
+rockButton.addEventListener("click", () => {
+  playRound("Rock", getComputerChoice());
+});
+
+// paperButton.addEventListener("click", () => {
+//   alert("Hello World");
+// });
+// scissorsButton.addEventListener("click", () => {
+//   alert("Hello World");
+// });
 
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3)) {
@@ -58,14 +57,31 @@ function playRound(humanChoice, computerChoice) {
   console.log("PooPooCaCa played: " + computerChoice);
 } // Play a round & score
 
-function playGame() {
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  console.log("Your score: " + humanScore);
-  console.log("PooPooCaCa score: " + pooPooCaCaScore);
-}
+// function getHumanChoice() {
+//   let hoomanChoice = prompt(
+//     "Type 'Rock', 'Paper' or 'Scissors'",
+//     `${getComputerChoice()}`
+//   );
+//   switch (hoomanChoice.toLowerCase().trim()) {
+//     case "rock":
+//       return "Rock";
+//     case "paper":
+//       return "Paper";
+//     case "scissors":
+//       return "Scissors";
+//     default:
+//       alert("You didn't pick a valid option");
+//   }
+// } // Hooman choice
 
-playGame();
+// function playGame() {
+//   playRound(getHumanChoice(), getComputerChoice());
+//   playRound(getHumanChoice(), getComputerChoice());
+//   playRound(getHumanChoice(), getComputerChoice());
+//   playRound(getHumanChoice(), getComputerChoice());
+//   playRound(getHumanChoice(), getComputerChoice());
+//   console.log("Your score: " + humanScore);
+//   console.log("PooPooCaCa score: " + pooPooCaCaScore);
+// }
+
+// playGame();
